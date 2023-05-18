849901676a1e7d6b979904d558a77ae2cee48d61
Pat <patrick.sliz@gmail.com>
2023-05-18 17:55:56 -0400

    Wiener filter functional

--- cresta.py
+++ cresta.py
@@ -212,7 +212,9 @@ class Tabs(TabbedPanel):
 	#	wiener
 		if wienerbutton == True:
 			import mrcfile
-		#	from tomopy.deconv import deconvolution
+			import numpy as np
+			from scipy.interpolate import interp1d
+			from scipy.fftpack import fftn, ifftn
 
 			# remove current filtered .mrc files
 			filtered_files = [f for f in os.listdir(direct) if f.endswith("_filt.mrc")]
@@ -227,7 +229,58 @@ class Tabs(TabbedPanel):
 
 				# read .mrc file and apply filter
 				mrc = mrcfile.read(fullFileName)
-			#	subtomo_filt = deconvolution(mrc, size=mrc.shape[1], angpix=angpix, defocus_um=defocus, snr_ratio=snrratio)
+	
+				def tom_ctf1d(n, pixelsize, voltage, cs, defocus, amplitude_contrast, phase_shift, bfactor):
+					ny = 1 / (pixelsize)
+					lambda_ = 12.2643247 / np.sqrt(voltage * (1.0 + voltage * 0.978466e-6)) * 1e-10
+					lambda2 = lambda_ * 2
+
+					points = np.arange(0, n)
+					points = points / (2 * n) * ny
+					k2 = n**2
+
+					term1 = lambda_**3 * cs * k2**2
+					w = np.pi / 2 * (term1 + lambda2 * defocus * k2) - phase_shift
+
+					acurve = np.cos(w) * amplitude_contrast
+					pcurve = -np.sqrt(1 - amplitude_contrast**2) * np.sin(w)
+					bfactor_term = math.exp(-bfactor * k2 * 0.25)
+
+					ctf = (pcurve + acurve) * (bfactor_term)
+
+					return ctf
+				
+				def tom_deconv_tomo(vol, angpix, defocus, snrfalloff, highpassnyquist):
+					highpass = np.arange(0, 1, 1 / 2047)
+					highpass = np.minimum(1, highpass / highpassnyquist) * np.pi
+					highpass = 1 - np.cos(highpass)
+
+					snr = np.exp((np.arange(0, -1, -1 / 2047)) * snrfalloff * 100 / angpix) * 1000 * highpass
+					ctf = np.abs(tom_ctf1d(2048, angpix * 1e-10, 300e3, 2.7e-3, -defocus * 1e-6, 0.07, 0, 0))
+					wiener = ctf / (ctf * ctf + 1 / snr)
+
+					s1 = -np.floor(vol.shape[0] / 2)
+					f1 = s1 + vol.shape[0] - 1
+					s2 = -np.floor(vol.shape[1] / 2)
+					f2 = s2 + vol.shape[1] - 1
+					s3 = -np.floor(vol.shape[2] / 2)
+					f3 = s3 + vol.shape[2] - 1
+
+					x, y, z = np.mgrid[s1:f1+1, s2:f2+1, s3:f3+1]
+					x = x / np.abs(s1)
+					y = y / np.abs(s2)
+					z = z / max(1, np.abs(s3))
+					r = np.sqrt(x**2 + y**2 + z**2)
+					r = np.minimum(1, r)
+					r = np.fft.ifftshift(r)
+					x = np.arange(0, 1, 1 / 2047)
+
+					ramp = interp1d(x, wiener, kind='linear', fill_value='extrapolate')(r)
+					deconv = np.real(np.fft.ifftn(np.fft.fftn(vol.astype(np.float32)) * ramp))
+					return deconv
+				
+				subtomo_filt = tom_deconv_tomo(mrc, angpix=angpix, defocus=defocus, snrfalloff=snrratio, highpassnyquist=.01)
+				subtomo_filt = subtomo_filt.astype('float32')
 
 				# write filtered .mrc file
 				baseFileName, extension = os.path.splitext(f)
@@ -255,7 +308,7 @@ class Tabs(TabbedPanel):
 				# read .mrc file and apply filter
 				mrc = mrcfile.read(fullFileName)
 				subtomo_filt = gaussian_filter(mrc, sigma=sigval)
-					
+	
 				# write filtered .mrc file
 				baseFileName, extension = os.path.splitext(f)
 				newFileName = os.path.join(direct, baseFileName + '_filt.mrc')
